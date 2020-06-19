e645a17c29ea48be5e0e58bce3be98ed738f9e6a
Syed Abdul Quader <sydadder@gmail.com>
2020-06-19 13:56:38 +0100

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require("express");
 var router = express.Router();
 require.cache = {};
 
+router.get("/", function (req, res, next) {
+  res.render("games", {
+    title: "Game List",
+  });
+});
+
 router.get("/games", function (req, res, next) {
   res.render("games", {
     title: "Game List",
